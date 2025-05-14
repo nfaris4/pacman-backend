1b3a0084a0ae3da8c103d70eaf6525b47993998a
nfaris4 <nfaris@inscaparrella.cat>
2025-05-14 14:55:05 +0200

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/pacman-backend/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+});
